95943e306864eaba1188753ed03e95df39087561
gauthier.cornec <gauthier.cornec@gmail.com>
2020-11-09 14:59:15 +0100

    beginning of predator and prey implementation : variable & method declaration without any code yet

--- src/com/company/Predator.java
+++ src/com/company/Predator.java
@@ -1,4 +1,25 @@
 package com.company;
 
 public class Predator {
+
+    int AgeNow;
+    int AgeMax;
+    static final int AgeMaxAll = 40;
+
+    int NbLifePointsNow;
+    int NbLifePointsMax;
+    static final int NbLifePointsAll = 120;
+
+    Smelling smelling;
+    Vision vision;
+
+    public void hunt (){};
+    public void move (){};
+    public void eat (){};
+    public void drink (){};
+    public void mate (){};
+
+
+
+
 }
--- src/com/company/Prey.java
+++ src/com/company/Prey.java
@@ -1,4 +1,15 @@
 package com.company;
 
 public class Prey {
+
+    static int s_iMaxPreyAge = 30;
+    int m_iAgeNow;
+    int m_iAgeMax;
+
+    int NbLifePointNow;
+    int NbLifePointMax;
+
+    static final int s_iNbLifePointsMaxAll = 120;
+
+
 }
